fbd8de3f67ace11b96b9e87c2d35b7b03a9d0092
Kyle <meel0002@algonquinlive.com>
2018-04-05 09:24:40 -0400

    oc transpo activity added

--- app/src/main/java/com/example/kyle/android_final/MainActivity.java
+++ app/src/main/java/com/example/kyle/android_final/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kyle.android_final;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,46 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //OC Transpo open handler
+        Button btn = this.findViewById(R.id.btnOCTranspo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OCTranspoMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Intake form open handler
+        btn = this.findViewById(R.id.btnIntake);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, IntakeFormMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        //Movie info open handler
+        btn = this.findViewById(R.id.btnMovieInfo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, MovieInfoMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        //Multiple choice quiz open handler
+        btn = this.findViewById(R.id.btnMultipleChoice);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, MultipleChoiceMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/kyle/android_final/OCTranspoMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kyle.android_final;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OCTranspoMainActivity extends Activity {
+
+    private static final String APPID = "223eb5c3";
+    private static final String APIKEY = "ab27db5b435b8c8819ffb8095328e775";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_octranspo_main_activuty);
+    }
+}
